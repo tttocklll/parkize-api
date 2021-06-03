fe11673993badd1f091dddfdba5ea805b0c65f48
OHMURA Tokiya <tocky.dison@gmail.com>
2021-06-04 05:51:54 +0900

    feat: enhance register & search endpoint

--- get.ts
+++ get.ts
@@ -14,9 +14,46 @@ function doGet(e) {
 }
 
 function register(params) {
-  return { register: true };
+  try {
+    const sheet_id = PropertiesService.getScriptProperties().getProperty("SHEET_ID");
+    // TODO: シートが複数になっても動作するように
+    const sheet = SpreadsheetApp.openById(sheet_id).getSheets()[0];
+    const last_col = sheet.getLastColumn();
+    const last_row = sheet.getLastRow();
+    const indices = sheet.getRange(1, 1, 1, last_col).getValues()[0];
+
+    for (let i = 0; i < indices.length; i++) {
+      const index = indices[i]
+      if (params[index]) {
+        sheet.getRange(last_row + 1, i + 1).setValue(params[index]);
+      }
+    }
+    return { register: true };
+  } catch (error) {
+    Logger.log(error)
+  }
+
 }
 
 function search(params) {
-  return { search: true };
+  const targetNumber = params.car_number;
+
+  const sheet_id = PropertiesService.getScriptProperties().getProperty("SHEET_ID");
+  // TODO: シートが複数になっても動作するように
+  const sheet = SpreadsheetApp.openById(sheet_id).getSheets()[0];
+  const last_col = sheet.getLastColumn();
+  const last_row = sheet.getLastRow();
+  const sheetData = sheet.getRange(1, 1, last_row, last_col).getValues();
+
+  const indexOfCarNumber = sheetData[0].indexOf("car_number");
+
+  const result = [];
+  for (const item of sheetData) {
+    if (item[indexOfCarNumber] == targetNumber) {
+      result.push(item);
+    }
+    Logger.log(item[indexOfCarNumber])
+  }
+
+  return { search: result, indexOfCarNumber, targetNumber };
 }
