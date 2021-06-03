52d372b9770a2adc17c76e8f1d36e4bbad1fcfec
OHMURA Tokiya <tocky.dison@gmail.com>
2021-06-03 13:30:25 +0900

    feat: use doGet & mode

--- get.ts
+++ get.ts
@@ -1,4 +1,22 @@
 function doGet(e) {
-  Logger.log(e);
-  return ContentService.createTextOutput(JSON.stringify(e));
+  const params = e.parameter;
+
+  const mode = params.mode;
+
+  switch (mode) {
+    case "register":
+      return ContentService.createTextOutput(JSON.stringify(register(params)));
+    case "search":
+      return ContentService.createTextOutput(JSON.stringify(search(params)));
+    default:
+      break;
+  }
+}
+
+function register(params) {
+  return { register: true };
+}
+
+function search(params) {
+  return { search: true };
 }
--- post.ts
+++
@@ -1,4 +0,0 @@
-function doPost(e) {
-  Logger.log(e);
-  return ContentService.createTextOutput(JSON.stringify(e));
-}
