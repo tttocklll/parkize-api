7dd851a58330b39718620c814e10613f9ad3c903
OHMURA Tokiya <tocky.dison@gmail.com>
2021-06-07 17:15:17 +0900

    feat: add unique constraint

--- get.ts
+++ get.ts
@@ -15,30 +15,46 @@ function doGet(e) {
 
 function register(params) {
   try {
-    const sheet_id = PropertiesService.getScriptProperties().getProperty("SHEET_ID");
+    const sheet_id =
+      PropertiesService.getScriptProperties().getProperty("SHEET_ID");
     // TODO: シートが複数になっても動作するように
     const sheet = SpreadsheetApp.openById(sheet_id).getSheets()[0];
     const last_col = sheet.getLastColumn();
     const last_row = sheet.getLastRow();
-    const indices = sheet.getRange(1, 1, 1, last_col).getValues()[0];
+    const sheetData = sheet.getRange(1, 1, last_row, last_col).getValues();
+    const indices = sheetData[0];
+    const indexOfCarNumber = sheetData[0].indexOf("car_number");
+    const indexOfName = sheetData[0].indexOf("name");
+
+    for (const data of sheetData) {
+      if (
+        data[indexOfCarNumber] == params.car_number &&
+        data[indexOfName] == params.name
+      ) {
+        return {
+          success: false,
+          error: "同じナンバーと名前の人が登録されています",
+        };
+      }
+    }
 
     for (let i = 0; i < indices.length; i++) {
-      const index = indices[i]
+      const index = indices[i];
       if (params[index]) {
         sheet.getRange(last_row + 1, i + 1).setValue(params[index]);
       }
     }
-    return { register: true };
+    return { success: true };
   } catch (error) {
-    Logger.log(error)
+    Logger.log(error);
   }
-
 }
 
 function search(params) {
   const targetNumber = params.car_number;
 
-  const sheet_id = PropertiesService.getScriptProperties().getProperty("SHEET_ID");
+  const sheet_id =
+    PropertiesService.getScriptProperties().getProperty("SHEET_ID");
   // TODO: シートが複数になっても動作するように
   const sheet = SpreadsheetApp.openById(sheet_id).getSheets()[0];
   const last_col = sheet.getLastColumn();
@@ -52,7 +68,7 @@ function search(params) {
     if (item[indexOfCarNumber] == targetNumber) {
       result.push(item);
     }
-    Logger.log(item[indexOfCarNumber])
+    Logger.log(item[indexOfCarNumber]);
   }
 
   return { search: result, indexOfCarNumber, targetNumber };
